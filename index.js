5c420b84a0eb84935e659d5154e1dfe35d11003e
Özge Gündoğdu <ozgegundogduu92@gmail.com>
2019-06-17 22:33:25 +0300

    created action

--- index.js
+++ index.js
@@ -7,32 +7,47 @@ function createStore(reducer) {
   // 3. Listen to changes on the state.
   // 4. Update the state
 
-  let state; //1
-  let listeners = [];
+  let state //1
+  let listeners = []
 
-  const getState = () => state; //2
+  const getState = () => state //2
 
   //3
   const subscribe = listener => {
-    listener.push(listener);
+    listeners.push(listener)
     return () => {
-      listeners = listeners.filter(l => l !== listener);
-    };
-  };
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
 
   //4
   const dispatch = action => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  };
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
 
-  return { getState, subscribe, dispatch };
+  return { getState, subscribe, dispatch }
 }
 
 //App Code
 function todos(state = [], action) {
   if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
+    return state.concat([action.todo])
   }
-  return state;
+  return state
 }
+
+const store = createStore(todos)
+
+store.subscribe(() => {
+  console.log("The new state is: ", store.getState())
+})
+
+store.dispatch({
+  type: "ADD_TODO",
+  todo: {
+    id: 0,
+    name: "Learn Redux",
+    complete: false
+  }
+})
