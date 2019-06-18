71c9ea494966f894a2943e3a3346179f207b112f
Özge Gündoğdu <ozgegundogduu92@gmail.com>
2019-06-18 09:39:51 +0300

    ADD_GOALS, REMOVE_GOAL method is created

--- index.js
+++ index.js
@@ -31,17 +31,31 @@ function createStore(reducer) {
 
 //App Code
 function todos(state = [], action) {
-    switch(action.type) {
-        case 'ADD_TODO' :
-          return state.concat([action.todo])
-        case 'REMOVE_TODO' :
-          return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO' :
-          return state.map((todo) => todo.id !== action.id ? todo :
-            Object.assign({}, todo, { complete: !todo.complete }))
-        default :
-          return state
-      }
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map(todo =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal])
+    case "REMOVE_GOAL":
+      return state.filter(goal => goal.id !== action.id)
+    default:
+      return state
+  }
 }
 
 const store = createStore(todos);
